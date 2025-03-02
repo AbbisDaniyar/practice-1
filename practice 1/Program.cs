47194c4dd3ef3ea7fda5bec31b6f7cf37be6c776
lenovo <lenovo@DESKTOP-3P7056B>
2025-03-02 19:58:23 +0300

    Добавьте файлы проекта.

---
+++ practice 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using System.Linq;
+
+namespace practice_1
+{
+    internal class Program
+    {
+        static string ProcStr(string str)
+        {
+            if (str == null)
+            {
+                return str;
+            }
+
+            int strLength = str.Length;
+
+            if (strLength % 2 == 0) 
+            {
+                int midpoint = strLength / 2;
+                string str1 = str.Substring(0,midpoint);
+                string str2 = str.Substring(midpoint);
+                
+                str1= new string(str1.Reverse().ToArray());
+                str2 = new string(str2.Reverse().ToArray());
+
+                return str1+str2;
+            }
+            else
+            {
+                string reversstr = new string(str.Reverse().ToArray()); 
+                return reversstr+str;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string input = Console.ReadLine();
+            string output = ProcStr(input);
+            Console.WriteLine("Обработанная строка:" + output);
+        }
+    }
+}
